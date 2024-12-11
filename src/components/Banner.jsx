d9db8cfb0d1c988bf0acbd18c75158427434b7b4
MH <vmh26122004@gmail.com>
2024-12-11 21:02:54 +0700

    done page home

---
+++ src/components/Banner.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import '../assets/css/Home.css';
+
+function Banner() {
+
+    const bannerImg = [
+        '/images/banner1.webp',
+        '/images/banner2.webp',
+        '/images/banner3.webp',
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [slideDirection, setSlideDirection] = useState('');
+    const [isPaused, setIsPaused] = useState(false);
+
+    const nextImg = () => {
+        setSlideDirection('right');
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % bannerImg.length);
+    }
+
+    const prevImg = () => {
+        setSlideDirection('left');
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + bannerImg.length) % bannerImg.length);
+    }
+
+    useEffect(() => {
+        if(isPaused) return;
+        const interval = setInterval(() => {
+            nextImg();
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [isPaused])
+    return ( 
+        <div 
+            className="banner"
+            onMouseEnter={() => setIsPaused(true)} // dừng chuyển ảnh khi có sự kiện hover vào banner
+            onMouseLeave={() => setIsPaused(false)} // tiếp tục chuyển ảnh
+        >
+            <div className="buttons">
+                <button className="left-btn" onClick={prevImg}>
+                    <i class="fa-solid fa-chevron-left"></i>
+                </button>
+                <button className="right-btn" onClick={nextImg}>
+                    <i class="fa-solid fa-chevron-right"></i>
+                </button>
+            </div>
+            <div className={`slider ${slideDirection}`}>
+                <img 
+                    src={bannerImg[currentIndex]} 
+                    alt={`Banner ${currentIndex + 1}`}
+                    key={currentIndex}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
---
+++ src/components/BrandIcon.jsx
@@ -0,0 +1,28 @@
+function BrandIcon() {
+    return ( 
+        <div className="brand-icon">
+            <h2 className="header">
+                Thương hiệu hàng đầu thế giới tại House of Luggage
+            </h2>
+            <div className="logo-brand">
+                <img src="/images/brand1.webp" alt="brand1" />
+                <img src="/images/brand2.webp" alt="brand2" />
+                <img src="/images/brand3.webp" alt="brand3" />
+                <img src="/images/brand4.webp" alt="brand4" />
+                <img src="/images/brand5.webp" alt="brand5" />
+                <img src="/images/brand6.webp" alt="brand6" />
+                <img src="/images/brand7.webp" alt="brand7" />
+                <img src="/images/brand8.webp" alt="brand8" />
+                <img src="/images/brand9.webp" alt="brand9" />
+                <img src="/images/brand10.webp" alt="brand10" />
+            </div>
+            <div className="img-monopoly">
+                <img src="/images/brand.png" alt="brand" />
+                <img src="/images/monopoly.png" alt="monopoly" />
+                <img src="/images/guarantee.png" alt="guarantee" />
+            </div>
+        </div>
+    );
+}
+
+export default BrandIcon;
