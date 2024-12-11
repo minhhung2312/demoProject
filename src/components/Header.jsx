d977b3b09fe058312cf3ec0061950a4c216f52b9
Le Minh Tri <leminhtri260903@gmail.com>
2024-12-11 21:45:45 +0700

    Fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,28 @@
 import { Link, Route, Routes } from 'react-router-dom';
+
 import '../assets/css/Header.css'
 function Header() {
+    let isNavbarFixed = false; 
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.nav');
+  const currentScrollY = window.scrollY;
+  if (currentScrollY > 400) {
+    if (!isNavbarFixed) {
+      
+      navbar.classList.add('fixed');
+      setTimeout(() => navbar.classList.add('visible'), 100); 
+      isNavbarFixed = true;
+    }
+  } else {
+    if (isNavbarFixed) {
+      
+      navbar.classList.remove('visible');
+      setTimeout(() => navbar.classList.remove('fixed'), 300); 
+      isNavbarFixed = false;
+    }
+  }
+});
+
     return ( 
         <div>
             <div className='line'>MIỄN PHÍ VẬN CHUYỂN CHO ĐƠN HÀNG TỪ 1.500.000Đ</div>
@@ -9,10 +31,30 @@ function Header() {
                     <Link to='/Home'><img src="/icon/logo.png" alt="" /></Link>
                 </div>
                 <ul className="nav__Product">
-                    <li><Link to='/Branch'>BRANCH</Link></li>
-                    <li><Link to='/Vali'>VALI</Link></li>
-                    <li><Link to='/Balo'>BALO</Link></li>
-                    <li><Link to='/Handbag'>HANDBAG</Link></li>
+                    <li><Link to='/Branch'>BRANCH
+                        
+                    </Link></li>
+                    <li><Link to='/Vali'>VALI
+                        <ul className="sub_Product">
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                        </ul>
+                    </Link></li>
+                    <li><Link to='/Balo'>BALO
+                        <ul className="sub_Product">
+                            <li><Link>1</Link></li>
+                            <li><Link>2</Link></li>
+                            <li>3</li>
+                        </ul>
+                    </Link></li>
+                    <li><Link to='/Handbag'>HANDBAG
+                        <ul className="sub_Product">
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                        </ul>
+                    </Link></li>
                 </ul>
                 <ul className="nav__Link">
                     <li><Link to='/Contact'>CONTACT</Link></li>
@@ -26,7 +68,8 @@ function Header() {
                     <li><Link to='/Cart'><img src="/icon/cart.png" alt="" /></Link></li>
                 </ul>
             </nav>
-            <div className="discount">Nhập mã <span style={{color:'red', textDecoration: 'underline'}}>HOL10</span> giảm 10% tất cả sản phẩm</div>
+            <div className="discount">Nhập mã <span className='discount-code'>HOL10</span> giảm 10% tất cả sản phẩm</div>
+       
             <Routes>
              
             </Routes>
