07277322aee6f6ef29ce45c462fcc6af9b0f2894
Le Minh Tri <leminhtri260903@gmail.com>
2024-12-12 21:22:28 +0700

    fix Header and up Brand page

---
+++ src/api/brand-list.js
@@ -0,0 +1,39 @@
+export const brands = [
+    {
+      id: 1,
+      name: "/images/brand/Ace-name.png",
+      background: "/images/brand/Ace.png",
+      about: "",
+    },
+    {
+      id: 2,
+      name: "/images/brand/American-name.png",
+      background: "/images/brand/American.png",
+      about: "",
+    },
+    {
+      id: 3,
+      name: "/images/brand/Carlton-name.png",
+      background: "/images/brand/Carlton.png",
+      about: "",
+    },
+    {
+      id: 4,
+      name: "/images/brand/Delsey-name.png",  
+      background: "/images/brand/Delsey.png",
+      about: "",
+    },
+    {
+      id: 5,
+      name: "/images/brand/HighSierra-name.png",
+      background: "/images/brand/HighSierra.png",
+      about: "",
+    },
+    {
+      id: 6,
+      name: "/images/brand/Kamiliant-name.png",
+      background: "/images/brand/Kamiliant.png",
+      about: "",
+    },
+    
+  ];
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,93 +1,99 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from "react-router-dom";
 
-import '../assets/css/Header.css'
-import Home from '../pages/Home';
+import "../assets/css/Header.css";
+import Home from "../pages/Home";
+import Brand from "../pages/Brand";
 
 function Header() {
-    let isNavbarFixed = false; 
-window.addEventListener('scroll', () => {
-  const navbar = document.querySelector('.nav');
-  const currentScrollY = window.scrollY;
-  if (currentScrollY > 400) {
-    if (!isNavbarFixed) {
-      
-      navbar.classList.add('fixed');
-      setTimeout(() => navbar.classList.add('visible'), 100); 
-      isNavbarFixed = true;
+  let isNavbarFixed = false;
+  window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".nav");
+    const currentScrollY = window.scrollY;
+    if (currentScrollY > 400) {
+      if (!isNavbarFixed) {
+        navbar.classList.add("fixed");
+        setTimeout(() => navbar.classList.add("visible"), 100);
+        isNavbarFixed = true;
+      }
+    } else {
+      if (isNavbarFixed) {
+        navbar.classList.remove("visible");
+        setTimeout(() => navbar.classList.remove("fixed"), 300);
+        isNavbarFixed = false;
+      }
     }
-  } else {
-    if (isNavbarFixed) {
-      
-      navbar.classList.remove('visible');
-      setTimeout(() => navbar.classList.remove('fixed'), 300); 
-      isNavbarFixed = false;
-    }
-  }
-});
+  });
 
-    return ( 
-        <div>
-            <div className='line'>MIỄN PHÍ VẬN CHUYỂN CHO ĐƠN HÀNG TỪ 1.500.000Đ</div>
-            <nav className="nav">
-                <div className="nav__logo">
-                    <Link to='/'><img src="/icon/logo.png" alt="" /></Link>
-                </div>
-                <ul className="nav__Product">
-
-                    <li><Link to='/Branch'>BRANCH
-                        
-                    </Link></li>
-                    <li><Link to='/Vali'>VALI
-                        <ul className="sub_Product">
-                            <li>1</li>
-                            <li>2</li>
-                            <li>3</li>
-                        </ul>
-                    </Link></li>
-                    <li><Link to='/Balo'>BALO
-                        <ul className="sub_Product">
-                            <li><Link>1</Link></li>
-                            <li><Link>2</Link></li>
-                            <li>3</li>
-                        </ul>
-                    </Link></li>
-                    <li><Link to='/Handbag'>HANDBAG
-                        <ul className="sub_Product">
-                            <li>1</li>
-                            <li>2</li>
-                            <li>3</li>
-                        </ul>
-                    </Link></li>
-
-                    <li><Link to='/Brand'>BRAND</Link></li>
-                    <li><Link to='/Vali'>VALI</Link></li>
-                    <li><Link to='/Balo'>BALO</Link></li>
-                    <li><Link to='/Handbag'>HANDBAG</Link></li>
-
-                </ul>
-                <ul className="nav__Link">
-                    <li><Link to='/Contact'>CONTACT</Link></li>
-                    <li><Link to='About'>ABOUT US</Link></li>
-                    <li><Link to='Login'>LOGIN</Link></li>
-                    <li><Link to='Signup'>SING UP</Link></li>
-                </ul>
-                <ul>
-                    <li><Link to='/Account'><img src="/icon/user.png" alt="" /></Link></li>
-                    <li><Link><img src="/icon/search.png" alt="" /></Link></li>
-                    <li><Link to='/Cart'><img src="/icon/cart.png" alt="" /></Link></li>
-                </ul>
-            </nav>
-
-            <div className="discount">Nhập mã <span className='discount-code'>HOL10</span> giảm 10% tất cả sản phẩm</div>
-       
-
-            <div className="discount">Nhập mã <span > HOL10 </span> giảm 10% tất cả sản phẩm</div>
-
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-            </Routes>
+  return (
+    <div>
+      <div className="line">MIỄN PHÍ VẬN CHUYỂN CHO ĐƠN HÀNG TỪ 1.500.000Đ</div>
+      <nav className="nav">
+        <div className="nav__logo">
+          <Link to="/">
+            <img src="/icon/logo.png" alt="" />
+          </Link>
         </div>
-     );
+        <ul className="nav__Product">
+          
+          <li>
+            <Link to="/Brand">BRAND</Link>
+          </li>
+          <li>
+            <Link to="/Vali">VALI</Link>
+          </li>
+          <li>
+            <Link to="/Balo">BALO</Link>
+          </li>
+          <li>
+            <Link to="/Handbag">HANDBAG</Link>
+          </li>
+        </ul>
+        <ul className="nav__Link">
+          <li>
+            <Link to="/Contact">CONTACT</Link>
+          </li>
+          <li>
+            <Link to="/About">ABOUT US</Link>
+          </li>
+          <li>
+            <Link to="/Login">LOGIN</Link>
+          </li>
+          <li>
+            <Link to="/Signup">SING UP</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/Account">
+              <img src="/icon/user.png" alt="" />
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <img src="/icon/search.png" alt="" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/Cart">
+              <img src="/icon/cart.png" alt="" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="discount">
+        Nhập mã <span className="discount-code">HOL10</span> giảm 10% tất cả sản
+        phẩm
+      </div>
+
+      
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Brand" element={<Brand></Brand>}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default Header;
