79db1c556d9447ca16df717b5db1b15d42fcbe22
Le Minh Tri <leminhtri260903@gmail.com>
2024-12-15 06:03:55 +0700

    Up page About

---
+++ src/pages/About.jsx
@@ -0,0 +1,34 @@
+import '../assets/css/About.css'
+// import '../App.css'
+function About() {
+    return ( 
+        <div>
+            <div className="container">
+                <h1 className='info'>Giới thiệu về House Of Luggage</h1>
+                <div className="content">
+                    <h2 className='title'>House Of Luggage (HOL.com.vn) - Nơi khởi đầu hành trình của bạn</h2>
+                    <p className="paragraph">Hơn 20 năm đại diện và phát triển được hàng triệu lượt khách tin yêu và mua sắm,<span className='highline'>House Of Luggage</span> tự hào là nơi duy nhất tại Việt Nam đại diện độc quyền các thương hiệu quốc tế về Vali, Balo, Túi xách, Đồng hồ và Phụ kiện có nguồn gốc rõ ràng, uy tín và đang thịnh hành nhất trên thế giới hiện nay như:</p>
+                    <ul className='list-item'>
+                        <li>(USA) <b>Samsonite</b> - <a href="https://samsonite-vietnam.com/" target='blank'>www.samsonite.com</a>;<b>American Tourister</b> - <a href="https://shop.americantourister.com/countrylinks.html">www.americantourister.com</a>; <b>High Sierra</b> - www.highsierra.com; <b>Kamiliant</b> - <a href="https://www.kamiliant.com/">www.kamiliant.com</a> ; <b>Traveler's Choice</b> - <a href="https://www.travelerchoice.com/"> www.travelerchoice.com</a></li>
+                        <li>(Japan) <b>ACE</b> - <a href="https://www.ace-dot.com/"> www.ace-dot.com</a></li>
+                        <li>(England) <b>Carlton</b> - <a href="https://movewithcarlton.com/">www.carltontravelgoods.com</a></li>
+                        <li>(France) <b>Delsey</b>- <a href="https://fr.delsey.com/">www.delsey.com</a>;<b>Lipault</b> - <a href="http://lipault.com/">www.lipault.com</a> </li>
+                        <li>(Swiss) <b>Victorinox</b>  -  <a href="https://www.victorinox.com/en/">www.victorinox.com</a>; <b>Wenger</b>  - <a href="https://www.wenger.ch/global/en"> www.wenger.ch</a></li>
+                        <li>(Australia) <b>Pacsafe</b>  - <a href="https://pacsafe.com/">www.pacsafe.com</a> </li>
+                        <li>(India) <b>Skybags</b>  - <a href="https://skybags.co.in/"> www.skybags.co.in</a></li>
+                    </ul>
+                    
+                    <p className="paragraph">Với tiêu chí mang đến các sản phẩm từ các thương hiệu hành lý quốc tế nổi tiếng, đẳng cấp và chất lượng, với mức giá hợp lý nhất đến cho người Việt Nam. Sản phẩm tại <span className='highline'>House Of Luggage</span> đều là sản phẩm chính hãng, độc quyền phân phối và được bảo hành quốc tế (lên đến 10 năm - hoặc vĩnh viễn), phù hợp với nhiều phân khúc khách hàng, đa dạng kiểu dáng và màu sắc cho bạn dễ dàng lựa chọn. Đến với <span className='highline'>House Of Luggage</span>, chắc chắn bạn sẽ tìm được sản phẩm ưng ý nhất cho hành trình của mình.</p>
+                    <p className="paragraph"><span className='highline'>House Of Luggage</span> hiện đã có mặt tại các trung tâm thương mại uy tín trên toàn quốc, chắc chắn sẽ mang đến cho bạn những trải nghiệm mua sắm tuyệt vời với dịch vụ khách hàng, tư vấn sản phẩm tận tình, tiếp nhận bảo hành đúng quy chuẩn quốc tế, đảm bảo chế độ hậu mãi tốt nhất cho khách hàng khắp Việt Nam.</p>
+                    <p className="paragraph">Hãy cùng ghé thăm và mua sắm tại hệ thống của <span className='highline'>House Of Luggage</span> - Nơi khởi đầu hành trình của bạn, để những chuyến đi luôn là những trải nghiệm đầy mới mẻ và thú vị nhé!</p>
+                    <ul className="list-item">
+                        <li>Tham khảo sản phẩm và đặt hàng online tại website: hol.com.vn</li>
+                        <li>Tổng đài tư vấn miễn phí: 1800 6063</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default About;
