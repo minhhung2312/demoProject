85554ca98a3c48142e7df4d85615a281e72ca5ea
leminhtri <leminhtri260903@gmail.com>
2024-12-20 09:55:34 +0700

    translate vietnam to engish ace

--- src/pages/Ace.jsx
+++ src/pages/Ace.jsx
@@ -1,165 +1,155 @@
 import "../assets/css/Ace.css";
 
 function Ace() {
-    return (
-        <div className="ace">
-            <div className="intro">
-                <div className="intro-item">
-                    <div className="video">
-                        <iframe
-                            width="615"
-                            height="415"
-                            src="https://www.youtube.com/embed/eszmq10Z_T8"
-                            title="ace. TV commercial (Eng. Version_30 seconds)"
-                            frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            referrerpolicy="strict-origin-when-cross-origin"
-                            allowfullscreen
-                        ></iframe>
-                    </div>
-                </div>
-                <div className="intro-item">
-                    <div className="title">
-                        <img src="/images/ace-logo.webp" alt="Ace" />
-                        <p>
-                            Nguồn cảm hứng bất tận từ tinh hoa văn hóa Nhật Bản
-                        </p>
-                    </div>
-                    <div className="body">
-                        <div className="row">
-                            <img src="/images/ace01.webp" alt="" />
-                            <img src="/images/ace02.jpg" alt="" />
-                            <img src="/images/ace03.jpg" alt="" />
-                        </div>
-                        <div className="row">
-                            <img src="/images/ace04.webp" alt="" />
-                            <img src="/images/ace05.jpg" alt="" />
-                            <img src="/images/ace06.webp" alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="content">
-                <p className="paragraph">
-                    Khởi nguồn từ mong muốn mang đến "bạn đồng hành lý tưởng"
-                    giúp cho mỗi hành trình, mọi di chuyển đều trở nên thoải mái
-                    hơn. Và khái niệm ấy được hiện thực hóa qua từng bộ sưu tập
-                    vali, balo, túi xách của ace. không chỉ bởi tính năng, mà
-                    còn là những giá trị cảm xúc, thẩm mỹ, cùng tinh thần Nhật
-                    Bản.
-                </p>
-                <p className="paragraph">
-                    Sự tinh tế trong mỗi thiết kế của ace. luôn toát lên thần
-                    thái đáng kinh ngạc, thể hiện rõ những nét tinh hoa văn hóa
-                    của xứ sở Phù Tang.
-                </p>
-            </div>
-            <div className="info-1">
-                <div className="content">
-                    <div className="info-1-item">
-                        <h2 className="title">
-                            Đường nét hoàn mỹ, thiết kế ấn tượng
-                        </h2>
-                        <div className="body">
-                            <p>
-                                Cũng như đất nước và con người Nhật Bản, thương
-                                hiệu ace. luôn hướng tới sự hoàn mỹ trong từng
-                                chi tiết. Tất cả bộ sưu tập vali, balo, túi xách
-                                đều ra đời dựa trên khái niệm “Đường nét hoàn
-                                mỹ, thiết kế ấn tượng”, với sự giao hòa giữa giá
-                                trị khoa học và nghệ thuật chính thống.
-                            </p>
-                            <p>
-                                Với ace., thiết kế một chiếc vali, balo hay túi
-                                xách cũng giống như chế tác một mẫu trang sức,
-                                luôn đòi hỏi sự tinh tế, tỉ mỉ và cân bằng nhiều
-                                yếu tố.
-                            </p>
-                            <p>
-                                Tự hào mang đến những thiết kế độc đáo và chất
-                                lượng đỉnh cao, đó là lý do sản phẩm của ace. có
-                                thể chinh phục được mọi khách hàng trên toàn
-                                cầu.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="info-1-item">
-                        <img src="/images/info-1-img.webp" alt="" />
-                    </div>
-                </div>
-            </div>
-            <div className="info-2">
-                <div className="content">
-                    <img src="/images/ace08.jpg" alt="" />
-                    <img src="/images/ace09.webp" alt="" />
-                </div>
-            </div>
-            <div className="info-3">
-                <div className="content">
-                    <div className="info-3-item">
-                        <img src="/images/ace10.webp" alt="" />
-                    </div>
-                    <div className="info-3-item">
-                        <h2 className="title">
-                            ace. (ace dot) - Kết nối quá khứ, hiện tại và tương
-                            lai
-                        </h2>
-                        <div className="body">
-                            <p>
-                                Với sứ mệnh mang đến những sản phẩm hoàn mỹ,
-                                giúp mọi người di chuyển thoải mái hơn dựa trên
-                                giá trị cốt lõi từ sự khéo léo, tỉ mỉ và tài
-                                nghệ sáng tạo của các bậc thầy thiết kế, cùng độ
-                                chính xác trong kỹ thuật.
-                            </p>
-                            <p>
-                                Mỗi sản phẩm của ace. được sinh ra dựa trên nền
-                                tảng kỹ thuật, bí quyết đã được xây dựng, tích
-                                lũy gần 100 năm, kết hợp yếu tố thẩm mỹ và nhu
-                                cầu thực tiễn của người tiêu dùng để sáng tạo
-                                nên những thiết kế hoàn mỹ.
-                            </p>
-                            <img src="/images/ace11.webp" alt="" />
-                            <p>
-                                Dấu chấm (.) đặc biệt phía sau tên thương hiệu
-                                chính là biểu tượng cho tinh thần ấy, là cách
-                                thức mà ace. đi từ quá khứ đến hiện tại, từ nơi
-                                này đến nơi khác và kết nối mọi người lại với
-                                nhau, tạo ra con đường duy nhất nối liền quá
-                                khứ, hiện tại tới tương lai.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="info-4">
-                <div className="content">
-                    <h2 className="title">
-                        ace. | Thương hiệu vali, balo, túi xách danh tiếng thuộc
-                        Tập đoàn ACE
-                    </h2>
-                    <p>
-                        Khởi đầu từ những cửa hàng bán lẻ túi xách mang tên
-                        Shinkawaryu ở Osaka (Nhật Bản), sau gần 80 năm hình
-                        thành và phát triển, ACE trở thành một trong những tập
-                        đoàn sản xuất, phân phối sản phẩm vali, balo, túi xách,
-                        cặp kéo và phụ kiện du lịch hàng đầu thế giới.
-                    </p>
-                    <p>
-                        Không ngừng đổi mới và phát triển, ACE từng bước vươn
-                        lên khẳng định vị thế của mình, trở thành tập đoàn uy
-                        tín toàn cầu khi sở hữu 4 thương hiệu nổi tiếng, và ace.
-                        là thương hiệu nổi bật trong số đó.
-                    </p>
-                    <p>
-                        Để biết thêm thông tin chi tiết, vui lòng truy cập
-                        website: www.ace-dot.com hoặc www.acejpn.com
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="ace">
+      <div className="intro">
+        <div className="intro-item">
+          <div className="video">
+            <iframe
+              width="615"
+              height="415"
+              src="https://www.youtube.com/embed/eszmq10Z_T8"
+              title="ace. TV commercial (Eng. Version_30 seconds)"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
+          </div>
         </div>
-    );
+        <div className="intro-item">
+          <div className="title">
+            <img src="/images/ace-logo.webp" alt="Ace" />
+            <p>Endless inspiration from the essence of Japanese culture</p>
+          </div>
+          <div className="body">
+            <div className="row">
+              <img src="/images/ace01.webp" alt="" />
+              <img src="/images/ace02.jpg" alt="" />
+              <img src="/images/ace03.jpg" alt="" />
+            </div>
+            <div className="row">
+              <img src="/images/ace04.webp" alt="" />
+              <img src="/images/ace05.jpg" alt="" />
+              <img src="/images/ace06.webp" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="content">
+        <p className="paragraph">
+          Originating from the desire to bring an "ideal companion" to make
+          every journey and every move more comfortable, this concept is
+          realized through each collection of suitcases, backpacks, and bags by
+          Ace. It's not just about functionality but also emotional value,
+          aesthetics, and the spirit of Japan.
+        </p>
+        <p className="paragraph">
+          The sophistication in each design by Ace. always exudes an astonishing
+          aura, clearly reflecting the cultural essence of the Land of the
+          Rising Sun.
+        </p>
+      </div>
+      <div className="info-1">
+        <div className="content">
+          <div className="info-1-item">
+            <h2 className="title">Perfect contours, striking designs</h2>
+            <div className="body">
+              <p>
+                Like the country and people of Japan, the Ace. brand always
+                aspires to perfection in every detail. Every collection of
+                suitcases, backpacks, and bags is born from the concept of
+                "Perfect contours, striking designs," blending the values of
+                science with traditional artistry.
+              </p>
+              <p>
+                For Ace., designing a suitcase, backpack, or bag is akin to
+                crafting a piece of jewelry—it demands sophistication,
+                meticulous attention to detail, and a harmonious balance of
+                various elements.
+              </p>
+              <p>
+                Proud to deliver unique designs and top-notch quality, Ace.
+                products are able to win over customers worldwide.
+              </p>
+            </div>
+          </div>
+          <div className="info-1-item">
+            <img src="/images/info-1-img.webp" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="info-2">
+        <div className="content">
+          <img src="/images/ace08.jpg" alt="" />
+          <img src="/images/ace09.webp" alt="" />
+        </div>
+      </div>
+      <div className="info-3">
+        <div className="content">
+          <div className="info-3-item">
+            <img src="/images/ace10.webp" alt="" />
+          </div>
+          <div className="info-3-item">
+            <h2 className="title">
+              Ace. (Ace dot) - Connecting the past, present, and future
+            </h2>
+            <div className="body">
+              <p>
+                With a mission to deliver flawless products that make traveling
+                more comfortable, Ace. is rooted in core values of
+                craftsmanship, meticulous attention to detail, creative mastery,
+                and precision engineering.
+              </p>
+              <p>
+                Each product of ace. is created based on technical foundations
+                and expertise that have been built and accumulated over nearly
+                100 years, combining aesthetic elements and practical consumer
+                needs to craft perfect designs.
+              </p>
+
+              <img src="/images/ace11.webp" alt="" />
+              <p>
+                The distinctive dot (.) at the end of the brand name symbolizes
+                that spirit; it represents how ace. moves from the past to the
+                present, from one place to another, connecting people and
+                creating a unique path that bridges the past, present, and
+                future.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="info-4">
+        <div className="content">
+          <h2 className="title">
+            ace. | Renowned brand of luggage, backpacks, and bags under ACE
+            Corporation
+          </h2>
+
+          <p>
+            Starting from retail bag stores named Shinkawaryu in Osaka, Japan,
+            after nearly 80 years of establishment and development, ACE has
+            become one of the world's leading corporations in manufacturing and
+            distributing luggage, backpacks, bags, rolling cases, and travel
+            accessories.
+          </p>
+          <p>
+            Continuously innovating and growing, ACE has steadily risen to
+            affirm its position, becoming a globally recognized corporation with
+            ownership of four renowned brands, among which ace. stands out as a
+            prominent brand.
+          </p>
+          <p>
+            For more information, please visit the websites: www.ace-dot.com or
+            www.acejpn.com
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Ace;
