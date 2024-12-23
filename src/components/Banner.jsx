b11c850881461fff3c945d69c921131985f8ad13
MH <vmh26122004@gmail.com>
2024-12-23 14:09:05 +0700

    update path img

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,56 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import '../assets/css/Home.css';
+import React, { useState, useEffect } from "react";
+import "../assets/css/Home.css";
 
 function Banner() {
-
     const bannerImg = [
-        '/images/banner1.webp',
-        '/images/banner2.webp',
-        '/images/banner3.webp',
+        `${process.env.PUBLIC_URL}/images/banner1.webp`,
+        `${process.env.PUBLIC_URL}/images/banner2.webp`,
+        `${process.env.PUBLIC_URL}/images/banner3.webp`,
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [slideDirection, setSlideDirection] = useState('');
+    const [slideDirection, setSlideDirection] = useState("");
     const [isPaused, setIsPaused] = useState(false);
 
     const nextImg = () => {
-        setSlideDirection('right');
+        setSlideDirection("right");
         setCurrentIndex((prevIndex) => (prevIndex + 1) % bannerImg.length);
-    }
+    };
 
     const prevImg = () => {
-        setSlideDirection('left');
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + bannerImg.length) % bannerImg.length);
-    }
+        setSlideDirection("left");
+        setCurrentIndex(
+            (prevIndex) => (prevIndex - 1 + bannerImg.length) % bannerImg.length
+        );
+    };
 
-    const [guestCount, setGuestCount] = useState(Math.floor(Math.random() * 500) + 1);
+    const [guestCount, setGuestCount] = useState(
+        Math.floor(Math.random() * 500) + 1
+    );
 
     useEffect(() => {
         const interval = setInterval(() => {
             const isIncrement = Math.random() > 0.4;
 
-            if(isIncrement) {
+            if (isIncrement) {
                 const increment = Math.floor(Math.random() * 50) + 1;
                 setGuestCount((prevCount) => prevCount + increment);
             } else {
                 const decrement = Math.floor(Math.random() * 30) + 1;
-                setGuestCount((prevCount) => Math.max(prevCount - decrement), 59);
+                setGuestCount(
+                    (prevCount) => Math.max(prevCount - decrement),
+                    59
+                );
             }
-
-        }, 9000)
-        return () => clearInterval(interval) 
-    }, [])
+        }, 9000);
+        return () => clearInterval(interval);
+    }, []);
 
     useEffect(() => {
-        if(isPaused) return;
+        if (isPaused) return;
         const interval = setInterval(() => {
             nextImg();
         }, 5000);
 
         return () => clearInterval(interval);
-    })
-    return ( 
-        <div 
+    });
+    return (
+        <div
             className="banner"
             onMouseEnter={() => setIsPaused(true)} // dừng chuyển ảnh khi có sự kiện hover vào banner
             onMouseLeave={() => setIsPaused(false)} // tiếp tục chuyển ảnh
@@ -58,7 +63,7 @@ function Banner() {
             <div className="guest">
                 <i className="fa-solid fa-eye"></i>
                 <p className="number">{guestCount}</p>
-            </div>   
+            </div>
             <div className="buttons">
                 <button className="left-btn" onClick={prevImg}>
                     <i className="fa-solid fa-chevron-left"></i>
@@ -68,8 +73,8 @@ function Banner() {
                 </button>
             </div>
             <div className={`slider ${slideDirection}`}>
-                <img 
-                    src={bannerImg[currentIndex]} 
+                <img
+                    src={bannerImg[currentIndex]}
                     alt={`Banner ${currentIndex + 1}`}
                     key={currentIndex}
                 />
--- src/components/BrandIcon.jsx
+++ src/components/BrandIcon.jsx
@@ -1,30 +1,69 @@
 function BrandIcon() {
-    return ( 
+    return (
         <div className="brand-icon">
             <h2 className="header">
-            World's Leading Brands at House of Luggage
+                World's Leading Brands at House of Luggage
             </h2>
             <div className="logo-brand">
-                <img src="/images/brand1.webp" alt="brand1" />
-                <img src="/images/brand2.webp" alt="brand2" />
-                <img src="/images/brand3.webp" alt="brand3" />
-                <img src="/images/brand4.webp" alt="brand4" />
-                <img src="/images/brand5.webp" alt="brand5" />
-                <img src="/images/brand6.webp" alt="brand6" />
-                <img src="/images/brand7.webp" alt="brand7" />
-                <img src="/images/brand8.webp" alt="brand8" />
-                <img src="/images/brand9.webp" alt="brand9" />
-                <img src="/images/brand10.webp" alt="brand10" />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand1.webp`}
+                    alt="brand1"
+                />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand2.webp`}
+                    alt="brand2"
+                />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand3.webp`}
+                    alt="brand3"
+                />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand4.webp`}
+                    alt="brand4"
+                />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand5.webp`}
+                    alt="brand5"
+                />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand6.webp`}
+                    alt="brand6"
+                />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand7.webp`}
+                    alt="brand7"
+                />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand8.webp`}
+                    alt="brand8"
+                />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand9.webp`}
+                    alt="brand9"
+                />
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/brand10.webp`}
+                    alt="brand10"
+                />
             </div>
             <div className="img-monopoly">
                 <div className="img-momopoly__item">
-                    <img src="/images/brand.png" alt="brand" />
+                    <img
+                        src={`${process.env.PUBLIC_URL}/images/brand.png`}
+                        alt="brand"
+                    />
                 </div>
                 <div className="img-momopoly__item">
-                    <img src="/images/monopoly.png" alt="monopoly" />
+                    <img
+                        src={`${process.env.PUBLIC_URL}/images/monopoly.png`}
+                        alt="monopoly"
+                    />
                 </div>
                 <div className="img-momopoly__item">
-                    <img src="/images/guarantee.png" alt="guarantee" />
+                    <img
+                        src={`${process.env.PUBLIC_URL}/images/guarantee.png`}
+                        alt="guarantee"
+                    />
                 </div>
             </div>
         </div>
